1ff8a28216238349a3106574b815c8ddbeadcf26
Deschandol Rémi <remi.deschandol@student-cs.fr>
2018-11-30 13:30:49 +0100

    working Player class with a add_music function

---
+++ Player.py
@@ -0,0 +1,21 @@
+import vlc
+
+
+class Player:
+    def __init__(self):
+        """initialize a player and set a path for the file it will read"""
+        self.instance = vlc.Instance()
+        self.music_player = self.instance.media_list_player_new()
+        self.media_list = self.instance.media_list_new([])
+        self.music_player.set_media_list(self.media_list)
+
+    def add_music(self, path):
+        """add a music at the end of the current Playlist"""
+        self.media_list.add_media(path)
+
+    def play(self):
+        """launch te player"""
+        self.music_player.play()
+
+    def set_volume(self, percentage):
+        self.music_player.audio_set_volume(percentage)
