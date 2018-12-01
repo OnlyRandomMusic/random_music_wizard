ad3616ed8163a19a8b1bdbc5d96f087bade1ed0f
Deschandol Rémi <remi.deschandol@student-cs.fr>
2018-12-01 16:39:00 +0100

    trying to know which song the player is currently playing

--- Player.py
+++ Player.py
@@ -5,9 +5,10 @@ class Player:
     def __init__(self):
         """initialize a player and set a path for the file it will read"""
         self.instance = vlc.Instance()
-        self.music_player = self.instance.media_list_player_new()
+        self.music_list_player = self.instance.media_list_player_new()
+        self.music_player = self.music_list_player.get_media_player()
         self.media_list = self.instance.media_list_new([])
-        self.music_player.set_media_list(self.media_list)
+        self.music_list_player.set_media_list(self.media_list)
 
     def add_music(self, path):
         """add a music at the end of the current Playlist"""
@@ -20,16 +21,17 @@ class Player:
 
     def play(self):
         """launch te player"""
-        self.music_player.play()
+        self.music_list_player.play()
 
     def next_music(self):
         """pass to the next music (never tested)"""
         print("[RASP] pass to the next music")
-        self.music_player.next()
+        self.music_list_player.next()
 
     def set_volume(self, percentage):
-        self.music_player.audio_set_volume(percentage)
+        self.music_list_player.audio_set_volume(percentage)
 
     def need_recharge(self):
         """doesn't work because media_list size isn't changed when a music is finished"""
+        print(self.music_list_player.audio_get_track_count())
         return self.media_list.count() < 10
