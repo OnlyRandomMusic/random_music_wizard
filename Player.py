b530758adc56da46ebe37d66cf8cfcca068ac444
Deschandol Rémi <remi.deschandol@student-cs.fr>
2018-12-04 19:15:17 +0100

    volume adjustment

--- Player.py
+++ Player.py
@@ -6,6 +6,7 @@ class Player:
         """initialize a player and set a path for the file it will read"""
         self.instance = vlc.Instance()
         self.music_player = self.instance.media_player_new()  # the class used to play the tracks
+        self.set_volume(0.6)
         self.current_music = 0  # the id of the music played
 
     def play_music(self, path):
@@ -14,14 +15,12 @@ class Player:
         song = self.instance.media_new(path)
         self.music_player.set_media(song)
         self.music_player.play()
-        print("hi")
 
     def set_volume(self, percentage):
         self.music_player.audio_set_volume(percentage)
 
     def music_ended(self):
         """called when the main detect that the song is finished"""
-        print(self.music_player.get_position())
         return self.music_player.get_position() == 1.
 
     #def check_if_track_changed(self):
