62ee39c5a8f20256751118edd31f7f6c1f305239
Deschandol Rémi <remi.deschandol@student-cs.fr>
2018-12-10 10:24:34 +0100

    database now functional

--- bdd.py
+++ bdd.py
@@ -2,20 +2,19 @@ import sqlite3
 
 
 def create():
-    conn = sqlite3.connect('.db')
-    c = conn.cursor()
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
 
     try:
-        c.execute('''CREATE TABLE music
+        cursor.execute('''CREATE TABLE music
                        (id, title_short, link, duration, r_date, preview_link, bpm, gain, artist_id,
                         album_id, path, downloaded)''')
 
-        c.execute('''CREATE TABLE album
-                        (id, genre_id, artist_id)''')
-
-        c.execute('''CREATE TABLE artist
-                        (id, name)''')
-
+        # cursor.execute('''CREATE TABLE album
+        #                 (id, genre_ids, artist_id)''')
+        #
+        # cursor.execute('''CREATE TABLE artist
+        #                 (id, name)''')
     except:
         print('base de donnée déja créée')
 
@@ -25,32 +24,39 @@ def create():
 
 def add_song(song, path=None, downloaded=False):
     conn = sqlite3.connect('example.db')
-    c = conn.cursor()
+    cursor = conn.cursor()
 
-    c.execute('SELECT * FROM music WHERE id=?', (song['id'],))
+    cursor.execute('SELECT * FROM music WHERE id=?', (song['id'],))
 
-    if not c.fetchone():
-        c.execute("INSERT INTO music VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", (
+    if not cursor.fetchone():
+        cursor.execute("INSERT INTO music VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", (
             song['id'], song['title_short'], song['link'], song['duration'], song['release_date'], song['preview'],
             song['bpm'], song['gain'], song['artist']['id'], song['album']['id'], path, downloaded))
 
         print("[RASP] Successfully added {} in database".format(song['title_short']))
     else:
         print("[RASP] Song {} already in database".format(song['title_short']))
+        return
 
     conn.commit()
     conn.close()
 
 
-def get_all_songs(print_title=False):
+def print_data():
     conn = sqlite3.connect('example.db')
-    c = conn.cursor()
-    c.execute('SELECT id, title_short FROM music')
-    data = c.fetchall()
-
-    if print_title:
-        for song in data:
-            print(song[1])
-
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM music')
+    data = cursor.fetchall()
     conn.close()
-    return [song[0] for song in data]
+
+    print(data)
+
+
+def reset():
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE IF EXISTS music')
+    cursor.execute('DROP TABLE IF EXISTS album')
+    cursor.execute('DROP TABLE IF EXISTS artist')
+    conn.commit()
+    conn.close()
