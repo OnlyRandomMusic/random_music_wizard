fee2245c7fa3a399722a88f983179cb28907a512
Deschandol Rémi <remi.deschandol@student-cs.fr>
2018-12-11 18:29:48 +0100

    adding verbose

--- deezer_load.py
+++ deezer_load.py
@@ -178,7 +178,7 @@ class Login:
         title = database.get_music_info(music_id, 'title')
 
         song = title + " - " + artist
-
+        print(song)
         # attention à cette ligne, elle est utile mais ne doit pas être oubliée lors de la génération du path
         # elle a pour but d'éviter les conflits si des caractères spéciaux sont présents dans les string
         dir = str(output) + "/" + artist.replace("/", "").replace("$", "S") + "/"
