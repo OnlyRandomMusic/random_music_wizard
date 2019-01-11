0333c6d51f3af244b9f9451ff2313628f27b5ffa
Deschandol Rémi <remi.deschandol@student-cs.fr>
2019-01-11 19:41:01 +0100

    adding a deploy script

---
+++ Web/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cp -f random_music_wizard/Web/index.html ../../var/www/html/
+cp -f random_music_wizard/Web/home.js ../../var/www/html/
+cp -f random_music_wizard/Web/home_style.css ../../var/www/html/
