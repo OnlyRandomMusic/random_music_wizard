ea023b5a384f8379be53e582bb5977fa55086709
Deschandol Rémi <remi.deschandol@student-cs.fr>
2019-01-12 10:51:34 +0100

    try to make it work not only in localhost

--- Web/home.js
+++ Web/home.js
@@ -9,3 +9,10 @@ function connect(){
 function play(){
 	socket.send('lalala');
 }
+
+function other_test(){
+    var io = require('socket.io-emitter')({ host: '10.57.167.107', port: 8484 });
+    setInterval(function(){
+        io.emit('time', new Date);
+    }, 5000);
+}
