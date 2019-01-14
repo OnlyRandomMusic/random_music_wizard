939991c3dbe7bf7f22239615d25727566efe924e
Deschandol Rémi <remi.deschandol@student-cs.fr>
2019-01-14 16:46:50 +0100

    making a playlist_database filler

--- Explorer.py
+++ Explorer.py
@@ -44,46 +44,6 @@ class Explorer:
 
         print("[RASP] Scores updated")
 
-    def playlist_brute_explore(self, n, m):
-        """make requests to deezer about playlist between id = n and id = m"""
-        identifier = n
-        while True:
-            try:
-                data = requests_tools.get_request("playlist/" + str(identifier), True)
-                if len(data['tracks']['data']) != 0:
-                    self.playlist_database.add_raw_playlist(data)
-            except:
-                print('fail')
-
-            identifier += 1
-            if identifier > m:
-                break
-
-    def playlist_random_explore(self, n):
-        """make requests to deezer about playlist randomly"""
-        for _ in range(n):
-            identifier = randint(10000, 2000000000)
-
-            try:
-                data = requests_tools.get_request("playlist/" + str(identifier), True)
-                if len(data['tracks']['data']) != 0:
-                    self.playlist_database.add_raw_playlist(data)
-            except:
-                print('fail')
-
-    def playlist_moderate_explore(self, identifier=0, step_size=50, sleep_time=10):
-        if identifier == 0:
-            identifier = self.playlist_database.get_raw_playlist_max_id()
-
-        while True:
-            # self.playlist_brute_explore(identifier, identifier + step_size)
-            # identifier += step_size
-            # print(identifier)
-
-            self.playlist_random_explore(step_size)
-
-            sleep(sleep_time)
-
 
 # from database import UserDatabase
 # m = MusicDatabase.MusicDatabase()
---
+++ database/playlist_database_filler.py
@@ -0,0 +1,69 @@
+from database import PlaylistDatabase
+import requests_tools
+from random import randint
+from time import sleep
+
+playlist_database = PlaylistDatabase.PlaylistDatabase()
+playlist_database.create()
+
+
+def playlist_brute_explore(n, m):
+    """make requests to deezer about playlist between id = n and id = m"""
+    identifier = n
+    while True:
+        try:
+            data = requests_tools.get_request("playlist/" + str(identifier), True)
+            if len(data['tracks']['data']) != 0:
+                playlist_database.add_raw_playlist(data)
+        except:
+            print('fail')
+
+        identifier += 1
+        if identifier > m:
+            break
+
+
+def playlist_random_explore(lowest_id, highest_id, n=50):
+    """make n requests to deezer about playlist randomly between id = a and id = b"""
+    success_nb = 0
+
+    for _ in range(n):
+        identifier = randint(lowest_id, highest_id)
+
+        try:
+            data = requests_tools.get_request("playlist/" + str(identifier), True)
+            if len(data['tracks']['data']) != 0:
+                playlist_database.add_raw_playlist(data)
+                success_nb += 1
+        except:
+            print('fail')
+
+    return success_nb
+
+
+def playlist_moderate_explore(identifier=0, step_size=50, sleep_time=10):
+    if identifier == 0:
+        identifier = playlist_database.get_raw_playlist_max_id()
+
+    while True:
+        # playlist_brute_explore(identifier, identifier + step_size)
+        # identifier += step_size
+        # print(identifier)
+
+        playlist_random_explore(step_size)
+
+        sleep(sleep_time)
+
+
+def measure_population(lowest_id, highest_id, step_size):
+    current_id = lowest_id
+    population = []
+
+    while current_id < highest_id:
+        nb_success = playlist_random_explore(current_id, current_id + step_size)
+        population.append((current_id, nb_success))
+        current_id += step_size
+
+    return population
+
+
