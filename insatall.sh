e3410fe81c73c34edf266fd1e61cca0effc72c33
rengati <remi.deschandol@orange.fr>
2019-02-05 15:07:48 +0100

    install bash file added

---
+++ insatall.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vlc
+sudo apt-get install virtualenv
+sudo apt-get install sqlite3
+
+bash short_install.sh
---
+++ short_install.sh
@@ -0,0 +1,9 @@
+rm -rf venv
+
+virtualenv venv
+source venv/bin/activate
+pip install requests
+pip install deezloader
+pip install simplejson
+pip install flask
+pip install python-vlc
