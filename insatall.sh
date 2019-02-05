d4c4e774001b1e9b45dd86510dbe7c1451d6bc64
rengati <remi.deschandol@orange.fr>
2019-02-05 15:13:32 +0100

    install bash file added

--- insatall.sh
+++ insatall.sh
@@ -1,8 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+sudo apt-get install apache2
 sudo apt-get install vlc
 sudo apt-get install virtualenv
 sudo apt-get install sqlite3
 
+bash deploy.sh
 bash short_install.sh
