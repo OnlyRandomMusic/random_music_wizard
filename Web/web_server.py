f1cbba3f9dc86ca4735faffe567e3c54df989eea
rengati <remi.deschandol@orange.fr>
2019-02-06 14:50:46 +0100

    new communication strategy

--- Web/web_server.py
+++ Web/web_server.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import render_template
 from multiprocessing.connection import Client
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
 
 @app.route('/you')
 def get_ses():
@@ -20,4 +22,4 @@ def get_ses():
     conn.send('lalalallalallalalalalalalala')
 
     print('done')
-    return home()
+    return 'hi'
