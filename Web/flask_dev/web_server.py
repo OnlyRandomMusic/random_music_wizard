cfccd5be68bc5af4284f4d1b701499bb72351600
Deschandol Rémi <remi.deschandol@student-cs.fr>
2019-02-09 15:44:27 +0100

    minimal webserver implemented

--- Web/flask_dev/web_server.py
+++ Web/flask_dev/web_server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from multiprocessing.connection import Client
+# from multiprocessing.connection import Client
 
 app = Flask(__name__)
 
@@ -12,14 +12,14 @@ def home():
 
 @app.route('/you')
 def get_ses():
-    try:
-        address = ('localhost', 6003)
-        conn = Client(address, authkey=b'secret password')
-    except:
-        address = ('localhost', 6004)
-        conn = Client(address, authkey=b'secret password')
-
-    conn.send('lalalallalallalalalalalalala')
+    # try:
+    #     address = ('localhost', 6003)
+    #     conn = Client(address, authkey=b'secret password')
+    # except:
+    #     address = ('localhost', 6004)
+    #     conn = Client(address, authkey=b'secret password')
+    #
+    # conn.send('lalalallalallalalalalalalala')
 
     print('done')
     return 'hi'
