dc8a66fcefd36a509dcb0ffa7bef1074d34d890d
rengati <remi.deschandol@orange.fr>
2019-02-10 10:53:09 +0100

    flask server test

--- Web/FLASKAPPS/helloworldapp/__init__.py
+++ Web/FLASKAPPS/helloworldapp/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
     return "Hello, Flask!"
+
+
+@app.route("/you/")
+def hello():
+    return "Hello, you!"
+
+
 if __name__ == "__main__":
     app.run()
