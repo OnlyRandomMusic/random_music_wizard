9b30b69bc5669fdf1722cc5c9f78d1f0e7c91f5e
rengati <remi.deschandol@orange.fr>
2019-02-11 19:28:33 +0100

    autoclear on search bar

--- web/web_flask_server/static/home.js
+++ web/web_flask_server/static/home.js
@@ -37,5 +37,6 @@ function like(){
 
 function search(){
     content = document.getElementById("search_bar").value;
+    document.getElementById("search_bar").value = '';
     request('/search/' + content)
 }
