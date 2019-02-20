cb29ffb0b09d782fb89f0cc8b2a449c8ba0bf541
rengati <remi.deschandol@orange.fr>
2019-02-21 00:04:18 +0100

    fan control added

--- fan_control.py
+++ fan_control.py
@@ -12,4 +12,9 @@ GPIO.setup(pin_number, GPIO.OUT)
 pwm = GPIO.PWM(pin_number, 100)  # 100 is the frequency of the PWM signal
 pwm.start(50)  # value in percent
 
-# pwm.ChangeDutyCycle(duty_cycle)
+duty_cycle = input()
+pwm.ChangeDutyCycle(duty_cycle)
+
+duty_cycle = input()
+
+pwm.ChangeDutyCycle(duty_cycle)
