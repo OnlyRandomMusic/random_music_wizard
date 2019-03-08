3c440396920dc5346948b6e3f65b6b2f8caa6805
rengati <remi.deschandol@orange.fr>
2019-03-08 22:07:55 +0100

    testing web socket

--- web_connection_test.py
+++ web_connection_test.py
@@ -1,6 +1,26 @@
-from communication import WebConnectionManager
+# coding: utf-8
 
-w = WebConnectionManager.WebConnectionManager()
-w.daemon = True
-w.start()
-input()
+import socket
+from time import sleep
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind(('', 15559))
+
+socket.listen(5)
+client, address = socket.accept()
+print("{} connected".format( address ))
+
+while True:
+    response = client.recv(255)
+    message = str(response, 'utf8')
+
+    if message != '':
+        print(message)
+
+    if message == 'close':
+        break
+
+
+print("Close")
+client.close()
+socket.close()
